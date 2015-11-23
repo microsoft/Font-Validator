c6a5c1c65832f3e9af88f8cabbd9697c49514204
Hin-Tak Leung <htl10@users.sourceforge.net>
2015-11-23 22:03:05 +0000

    fix problem of TTC DSIG mis-detected as garbage padding after tables

--- OTFontFile/OTFile.cs
+++ OTFontFile/OTFile.cs
@@ -197,6 +197,11 @@ namespace OTFontFile
             long PadFilePos = TableFilePos + TableLength;
 
             long NextTablePos = m_fs.Length;
+
+            // possible DSIG in TTC after all the tables
+            if (IsCollection() && (string) m_ttch.DsigTag == "DSIG" && m_ttch.DsigOffset > 0 && m_ttch.DsigOffset < NextTablePos && (string) t.m_tag != "DSIG" )
+                NextTablePos = m_ttch.DsigOffset;
+
             for (uint iFont=0; iFont<m_nFonts; iFont++)
             {
                 if (IsCollection())
